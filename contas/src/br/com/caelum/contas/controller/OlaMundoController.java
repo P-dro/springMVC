a284fbe2bf4a2b717cc5e2ecd4beefcf4ec0f92f
Pedro Ivo <38591069+P-dro@users.noreply.github.com>
2020-01-10 00:18:35 -0300

    Update OlaMundoController.java
    asdasd

--- contas/src/br/com/caelum/contas/controller/OlaMundoController.java
+++ contas/src/br/com/caelum/contas/controller/OlaMundoController.java
@@ -7,7 +7,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class OlaMundoController {
   @RequestMapping("/olaMundoSpring")
   public String execute() {
-    System.out.println("Executando a lógica com Spring MVC");
+    System.out.println("Executando a lÃ³gica com Spring MVC");
     return "ok";
   }
+  /*public static void main(String[] args) { SpringApplication.run(PedroSpringApplication.class, args);
+
+    String teste = "Teste";
+
+    Map<String, Integer> vehicles = new HashMap<>();
+
+    vehicles.put("BMW", 5);
+    vehicles.put("Mercedes", 3);
+    vehicles.put("Audi", 4);
+    vehicles.put("Ford", 10);
+
+    //String searchKey = "JJJ";
+    if (vehicles.containsKey("Ford"))
+        System.out.println("Found total " + vehicles.get("Ford") + " " + "Ford" + " cars!\n");
+
+    // Clear all values.
+    vehicles.clear();
+
+    // Equals to zero.
+    System.out.println("After clear operation, size: " + vehicles.size());
+}*/
 }
